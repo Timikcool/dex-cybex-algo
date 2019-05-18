16545891e110e5e4e3768a4e9beda554be8209dd
PaulsBecks <paul.beck.berlin@gmail.com>
2019-05-18 17:18:19 +0200

    added order call

--- src/OrderOverview/OrderOverview.js
+++ src/OrderOverview/OrderOverview.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {sellAlgoOrder} from "../services/cybex";
+import { sellAlgoOrder, buyAlgoOrder } from "../services/cybex";
 
 import "./OrderOverview.css";
 
@@ -15,12 +15,28 @@ class OrderOverview extends Component {
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSellAlgoOrder = this.handleSellAlgoOrder.bind(this);
   }
 
-  handleSellAlgoOrder() {
-    console.log("Called handleSellAlgoOrder")
-    sellAlgoOrder(this.state.assetPair, this.state.amount, this.state.price, 10, this.props.user)
+  handleAlgoOrder(type) {
+    switch (type) {
+      case "sell":
+        sellAlgoOrder(
+          this.state.assetPair,
+          this.state.amount,
+          this.state.price,
+          10,
+          this.props.user
+        );
+      case "buy":
+        buyAlgoOrder(
+          this.state.assetPair,
+          this.state.amount,
+          this.state.price,
+          10,
+          this.props.user
+        );
+      default:
+    }
   }
 
   handleInputChange(e) {
@@ -80,8 +96,8 @@ class OrderOverview extends Component {
 
         <br />
 
-        <button>Buy</button>
-        <button onClick={this.handleSellAlgoOrder}>Sell</button>
+        <button onClick={() => this.handleAlgoOrder("buy")}>Buy</button>
+        <button onClick={() => this.handleAlgoOrder("sell")}>Sell</button>
       </div>
     );
   }
